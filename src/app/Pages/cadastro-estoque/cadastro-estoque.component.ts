d64b9c35ec2e924a3def6369a09a332d165031c0
Vinicius Santos <vinicius.santos110vs@gmail.com>
2024-10-21 23:23:07 -0300

    criação tela cadastro

--- src/app/Pages/cadastro-estoque/cadastro-estoque.component.ts
+++ src/app/Pages/cadastro-estoque/cadastro-estoque.component.ts
@@ -1,35 +1,65 @@
 import { Component } from '@angular/core';
-
-interface Produto {
-  nome: string;
-  quantidade: number;
-  descricao: string;
-}
+import { ProdutoService } from '../../core/services/produto.service';
+import { Produto } from '../../types/Produtos';
 
 @Component({
   selector: 'app-cadastro-estoque',
   templateUrl: './cadastro-estoque.component.html',
-  styleUrls: ['./cadastro-estoque.component.css']
+  styleUrls: ['./cadastro-estoque.component.css'],
 })
 export class CadastroEstoqueComponent {
   produto: Produto = {
+    id: 0,
     nome: '',
+    descricao: '',
+    preco: 0,
+    imagem: '',
     quantidade: 0,
-    descricao: ''
   };
 
   estoque: Produto[] = [];
 
+  constructor(private produtoService: ProdutoService) {}
+
   cadastrarProduto() {
-    if (this.produto.nome && this.produto.quantidade > 0) {
-      this.estoque.push({ ...this.produto });
-      this.produto = { nome: '', quantidade: 0, descricao: '' }; // Limpa o formulário
-    } else {
-      alert('Por favor, preencha os campos corretamente.');
-    }
+    this.produtoService.cadastrar(this.produto).subscribe(
+      (response) => {
+        console.log('Produto cadastrado com sucesso', response);
+        // Atualiza o estoque local com a resposta do servidor
+        this.estoque.push(response);
+        this.resetarFormulario();
+      },
+      (error) => {
+        console.error('Erro ao cadastrar o produto', error);
+        alert('Erro ao cadastrar o produto. Tente novamente.'); // Mensagem de erro ao usuário
+      }
+    );
   }
 
   removerProduto(index: number) {
     this.estoque.splice(index, 1);
+  }
+
+  resetarFormulario() {
+    this.produto = {
+      id: 0,
+      nome: '',
+      descricao: '',
+      preco: 0,
+      imagem: '',
+      quantidade: 0,
+    };
+  }
+
+  onFileSelected(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      console.log('Arquivo selecionado:', file);
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.produto.imagem = reader.result as string; // Converte o arquivo para base64
+      };
+      reader.readAsDataURL(file);
+    }
   }
 }
